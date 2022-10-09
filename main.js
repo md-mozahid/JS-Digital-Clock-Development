63d9f564f4d73720255d3f3dd312b8a9c592a0c8
mozahid <ce.muzahid@gmail.com>
2022-10-09 16:41:06 +0600

    JS Digital Clock Development

---
+++ main.js
@@ -0,0 +1,21 @@
+function getCurrentTime() {
+  let time = new Date()
+  let hours = time.getHours()
+  let minutes = time.getMinutes()
+  let seconds = time.getSeconds()
+
+  let showBrowser = document.getElementById('clock')
+
+  hoursFormate = hours > 12 ? '0' + (hours - 12) : hours
+  minutesFormate = minutes < 10 ? '0' + minutes : minutes
+  secondsFormate = seconds < 10 ? '0' + seconds : seconds
+  amPmFormate = hours > 12 ? 'pm' : 'am'
+
+  let getValue = `${hoursFormate} : ${minutesFormate} : ${secondsFormate} ${amPmFormate}`
+
+  document.getElementById('clock').innerText = getValue
+}
+
+setInterval(() => {
+  getCurrentTime()
+}, 1000)
